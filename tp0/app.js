1721276175b6ade1e04e849585d9a01c3db9071d
Salim <bouzidsezlim@gmail.com>
2023-08-28 17:58:01 +0200

    first commit

---
+++ tp0/app.js
@@ -0,0 +1,21 @@
+const reasonInput =document.querySelector('#input-reason');
+const reasonAmount =document.querySelector('#input-amount');
+const cancelbtn =document.querySelector('#btn-cancel');
+const confirmbtn =document.querySelector('#btn-confirm');
+const expensesList = document.querySelector('#expenses-list');
+
+confirmbtn.addEventListener('click',()=> {
+    const enteredReason = reasonInput.value;
+    const entredAmount = reasonAmount.value
+    if(
+        enteredReason.trim().length <= 0 ||
+        entredAmount <=0 ||
+        entredAmount.trim().length <= 0
+        ){
+        return;
+
+    }
+    const newItem = document.createElement('ion-item');
+    newItem.textContent = enteredReason + ': $ '+ entredAmount
+    expensesList.appendChild(newItem);
+})
